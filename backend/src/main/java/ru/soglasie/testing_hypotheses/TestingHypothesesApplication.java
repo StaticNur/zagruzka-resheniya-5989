00d0c69cfadd25617b728acecab3eb9cd93957c5
Mukhammadnur <gsjxne@gmail.com>
2024-10-01 22:38:21 +0300

    init

---
+++ backend/src/main/java/ru/soglasie/testing_hypotheses/TestingHypothesesApplication.java
@@ -0,0 +1,13 @@
+package ru.soglasie.testing_hypotheses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingHypothesesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingHypothesesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/soglasie/testing_hypotheses/TestingHypothesesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.soglasie.testing_hypotheses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingHypothesesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
